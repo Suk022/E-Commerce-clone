e6b5ef654d3beff54285ba3a7c017c3aa793f482
Suk022 <122535400+Suk022@users.noreply.github.com>
2024-06-19 01:08:02 +0530

    Make Delivery option interactive

--- data/cart.js
+++ data/cart.js
@@ -47,5 +47,19 @@ export function removeFromCart(productId){
   });
 
   cart=newCart;
+ 
   saveToStorage();
+}
+
+export function updateDeliveyOption(productId,deliveryOptionId){
+  let matchingItem;
+  cart.forEach((cartItem)=>{
+    if(productId===cartItem.productId){
+      matchingItem=cartItem;
+    }
+   });
+
+   matchingItem.deliveryOptionId = deliveryOptionId;
+
+   saveToStorage();
 }
